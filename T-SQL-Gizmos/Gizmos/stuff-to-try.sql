86e90460f02d48d29c24dde52bd918a5bc2788b3
henrique <henrique.gaia@gmail.com>
2019-12-22 17:13:59 +0100

    Some notes

---
+++ T-SQL-Gizmos/Gizmos/stuff-to-try.sql
@@ -0,0 +1,9 @@
+/*
+When is the semocolon required
+
+There are two situations in which you must use the semicolon. 
+The first situation is where you use a Common Table Expression (CTE), 
+and the CTE is not the first statement in the batch. 
+The second is where you issue a Service Broker statement 
+and the Service Broker statement is not the first statement in the batch.
+*/
