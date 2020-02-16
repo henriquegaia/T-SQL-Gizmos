638fea5763e59330399282bf21608f26b5e8c2f8
henrique <henrique.gaia@gmail.com>
2020-02-16 13:15:44 +0100

    Blah

--- auto.ps1
+++ auto.ps1
@@ -1,23 +1,25 @@
-[console]::ForegroundColor = "white"
-[console]::BackgroundColor = "blue"
-
 git add .
 git commit -m "Blah"
 git pull origin master
 
-$CONFLICTS=$(git ls-files -u | wc -l)
+$CONFLICTS = (git ls-files -u | wc -l)
+
+[console]::ForegroundColor = "yellow"
+[console]::BackgroundColor = "black"
+Write-Host '==> conflicts: ' $CONFLICTS
+
 if ( $CONFLICTS -gt 0 ) {	
-	[console]::ForegroundColor = "green"
+	[console]::ForegroundColor = "red"
 	[console]::BackgroundColor = "black"
 	echo "There is a merge conflict. Aborting ..."
 	git merge --abort
 	exit 1
+}else{
+	git push origin master
+
+	[console]::ForegroundColor = "green"
+	[console]::BackgroundColor = "black"
+
+	Write-Host -NoNewLine 'Press any key to continue...';
+	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
-
-git push origin master
-
-[console]::ForegroundColor = "green"
-[console]::BackgroundColor = "black"
-
-Write-Host -NoNewLine 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
