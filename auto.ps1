a942a46ebf19b74681166a660e80428652cff9f7
henrique <henrique.gaia@gmail.com>
2020-02-16 11:40:43 +0100

    Blah

--- auto.ps1
+++ auto.ps1
@@ -2,3 +2,9 @@ git add .
 git commit -m "Blah"
 git pull origin master
 git push origin master
+
+[console]::ForegroundColor = "green"
+[console]::BackgroundColor = "black"
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
